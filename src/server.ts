85464e0fe0418573d9f381834aa23fda75019738
Daniel Ring <DanielKRing1@gmail.com>
2023-03-17 14:26:25 -0700

    Add .env file and package

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 // EXPRESS
 import express, { Express } from "express";
 import cors from "cors";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+console.log(process.env); // remove this after you've confirmed it is working
 
 // MIDDLEWARE
 import loggingMiddleware from "./middlewares/logging.middleware";
