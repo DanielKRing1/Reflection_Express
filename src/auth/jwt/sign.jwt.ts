ff97cb6522dccd9c6e8d45c89a6e985844d8b2eb
Daniel Ring <DanielKRing1@gmail.com>
2023-03-27 16:38:41 -0700

    Install jsonwebtoken + create verify+sign utils

---
+++ src/auth/jwt/sign.jwt.ts
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { Dict } from "../../types/global.types";
+
+type SignOptions = {
+  algorithm: jwt.Algorithm;
+  expiresIn: number;
+};
+export default async (
+  payload: Dict<any>,
+  secret: string,
+  { algorithm, expiresIn }: SignOptions = { algorithm: "HS256", expiresIn: 0 }
+) => {
+  // 1. Await token generation
+  const token: string | undefined = await new Promise<string | undefined>(
+    (res, rej) =>
+      jwt.sign(
+        { expiresIn, ...payload },
+        secret,
+        { algorithm },
+        function (err, token) {
+          if (err) return res(undefined);
+
+          return res(token);
+        }
+      )
+  );
+
+  // 2. Throw error if undefined token
+  if (token === undefined)
+    throw new Error("sign.jwt() produced an undefined token");
+
+  // 3. Return valid token
+  return token;
+};
