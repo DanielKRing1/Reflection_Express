40909bf557421ad3cb858bbeb4e78311292cdde2
Daniel Ring <DanielKRing1@gmail.com>
2023-03-30 22:06:45 -0700

    Remove unused express routes from router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,7 @@
 import { Router } from "express";
 
-import genInklingRouter from "./Inkling";
-import genReflectionRouter from "./Reflection";
-import genJournalRouter from "./Journal";
-import genJournalEntryRouter from "./JournalEntry";
-
 export default (): Router => {
   const router: Router = Router();
-
-  router.use("/inkling", genInklingRouter());
-  router.use("/reflection", genReflectionRouter());
-  router.use("/journal", genJournalRouter());
-  router.use("/journalEntry", genJournalEntryRouter());
 
   return router;
 };
