8217a7026c7530d64f2b33d57d220e750fb75a05
Daniel Ring <DanielKRing1@gmail.com>
2023-04-03 14:56:17 -0700

    Complete v1 of access + refresh flow

--- src/routes/Login/index.ts
+++ src/routes/Login/index.ts
@@ -1,30 +1,125 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import axios from "axios";
 
-import createUser from "../../controllers/Auth/createUser";
-import login from "../../controllers/Auth/login";
+import signJwt from "../../auth/jwt/sign.jwt";
+import compare from "../../auth/password/compare";
+
+import verifyJwtMiddleware from "../../middlewares/jwt/verifyJwt.middleware";
 import access from "../../middlewares/session/access";
+import { destroySession } from "../../middlewares/session/utils";
+import { Dict } from "../../types/global.types";
+import { HttpCookieResponse, mergeCookies } from "../../utils/cookies";
 
 export default (): Router => {
   const router: Router = Router({ mergeParams: true });
 
-  type LoginBody = {
-    username: string;
-    password: string;
-  };
-  router
-    .route("/create-user")
-    .post(async function (req: Request<{}, {}, LoginBody>, res: Response) {
-      const { username, password } = req.body;
-    });
-  router
-    .route("/")
-    .post(
-      access,
-      async function (req: Request<{}, {}, LoginBody>, res: Response) {}
-    );
-  router
-    .route("/get-refresh")
-    .post(access, async function (req: Request, res: Response) {});
+  router.route("/create-user").post(createUserController, loginController);
+  router.route("/").post(loginController);
+  router.route("/get-access").post(getAccessController);
 
   return router;
 };
+
+const createUserController = async function (
+  req: Request<{}, {}, LoginBody>,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { userId, password } = req.body;
+    if (userId === undefined || password === undefined)
+      throw new Error("/create-user received an undefined userId or password");
+
+    // 1. Create user in database if not exists
+    const creationStatus: boolean = true;
+    // if (creationStatus === false)
+    //   throw new Error("Use db error message as error message");
+
+    // 2. Login and get access and refresh tokens
+    next();
+  } catch (err) {
+    return res.status(409).send("Failed to create user");
+  }
+};
+
+type LoginBody = {
+  userId: string;
+  password: string;
+};
+const loginController = [
+  access,
+  async function (req: Request<{}, {}, LoginBody>, res: Response) {
+    try {
+      console.log("/login---------------------------");
+
+      // 1. Get user credentials
+      const { userId, password } = req.body;
+
+      // 2. Get User's password hash from db
+      const passwordHash: string = "";
+
+      // 3. Validate user credentials
+      // const isValid = await compare(password, passwordHash);
+      // // 4. Invalid, destroy session and return error code
+      // if (!isValid) {
+      //   throw new Error(
+      //     "/login controller determined that the provided user credentials are invalid"
+      //   );
+      // }
+
+      // 5. Create access session (and cookie)
+      req.session.userId = userId;
+
+      // 6. Valid, get refresh cookie
+      // 6.1. Create jwt
+      const payload: Dict<any> = {
+        userId,
+      };
+      const jwt: string = await signJwt(payload, process.env.JWT_SECRET!, {
+        expiresIn: 10, // Expires in 10 secs
+      });
+      // 6.2. Send jwt to '/refresh/get-refresh'
+      const responseWCookie = await axios.post(
+        // TODO Put this in config file
+        `http://localhost:4000/refresh/get-refresh`,
+        { jwt }
+      );
+
+      // 7. Add refresh cookie to res.cookie
+      mergeCookies(responseWCookie as HttpCookieResponse, res);
+
+      res.send("Success");
+    } catch (err) {
+      console.log(err);
+      // Destroy session on error
+      await destroySession(req);
+      return res.status(401).send("Unauthenticated");
+    }
+  },
+];
+
+type RefreshGetAccessBody = { jwt: string };
+const getAccessController = [
+  verifyJwtMiddleware,
+  access,
+  async function (req: Request<{}, {}, RefreshGetAccessBody>, res: Response) {
+    try {
+      console.log("/login/get-access---------------------------");
+
+      // 1. Get jwt
+      const jwtPayload = req.jwtPayload as Dict<any>;
+
+      // 2. Verify jwt
+      const { userId } = jwtPayload;
+
+      // 3. Add userId to refresh session
+      req.session.userId = userId;
+
+      return res.send("Look for cookie");
+    } catch (err) {
+      console.log(err);
+      await destroySession(req);
+      return res.status(401).send("Unauthenticated");
+    }
+  },
+];
--- src/routes/Refresh/index.ts
+++ src/routes/Refresh/index.ts
@@ -1,36 +1,97 @@
 import { Request, Response, Router } from "express";
 import axios from "axios";
 
-import refresh from "../../middlewares/session/refresh";
+import refresh, { maxAge } from "../../middlewares/session/refresh";
+import verifyJwtMiddleware from "../../middlewares/jwt/verifyJwt.middleware";
+import { Dict } from "../../types/global.types";
+import { destroySession } from "../../middlewares/session/utils";
+import signJwt from "../../auth/jwt/sign.jwt";
+import { mergeCookies, HttpCookieResponse } from "../../utils/cookies";
 
 export default (): Router => {
   const router: Router = Router({ mergeParams: true });
 
-  router.route("/").post(refresh, async function (req: Request, res: Response) {
-    // @ts-ignore
-    console.log(req.session.userId);
-    // @ts-ignore
-    req.session.userId = "test1";
-    // @ts-ignore
-    console.log(req.session.userId);
+  router.route("/").post(refreshController);
 
-    res.redirect("/refresh/get-access");
-  });
-  router.route("/get-access").post(
-    refresh,
-    async function (req: Request, res: Response) {},
-    function (req: Request, res: Response) {
-      // @ts-ignore
-      console.log(req.session.userId);
-      // @ts-ignore
-      req.session.userId = "test2";
-
-      // @ts-ignore
-      console.log(req.session.userId);
-
-      res.send("good");
-    }
-  );
+  router.route("/get-refresh").post(getRefreshController);
 
   return router;
 };
+
+type LoginGetRefreshBody = {
+  jwt: string;
+};
+const refreshController = [
+  refresh,
+  async function (req: Request<{}, {}, {}>, res: Response) {
+    try {
+      // 1. Check if has session
+      const { userId } = req.session;
+      if (userId === undefined)
+        throw new Error("/refresh received no session.userId");
+
+      // 2. Regenerate session if > half expired
+      console.log(
+        `Refresh cookie expires at: ${req.session?.cookie?.expires!}`
+      );
+      if (Date.now() - req.session?.cookie?.expires!.getTime() > maxAge / 2) {
+        await new Promise((reject, resolve) =>
+          req.session.regenerate((err) => {
+            if (err) return reject(err);
+            return resolve();
+          })
+        );
+      }
+
+      // 3. Get new access token
+      // 3.1. Create jwt
+      const payload: Dict<any> = {
+        userId,
+      };
+      const jwt: string = await signJwt(payload, process.env.JWT_SECRET!, {
+        expiresIn: 10, // Expires in 10 secs
+      });
+      // 3.2. Send jwt to '/login/get-access'
+      const responseWCookie = await axios.post(
+        // TODO Put this in config file
+        `http://localhost:4000/login/get-access`,
+        { jwt }
+      );
+
+      // 4. Add refresh cookie to res.cookie
+      mergeCookies(responseWCookie as HttpCookieResponse, res);
+
+      res.send("Success");
+    } catch (err) {
+      console.log(err);
+      // Destroy session on error
+      await destroySession(req);
+      return res.status(401).send("Unauthenticated");
+    }
+  },
+];
+
+const getRefreshController = [
+  verifyJwtMiddleware,
+  refresh,
+  async function (req: Request<{}, {}, LoginGetRefreshBody>, res: Response) {
+    try {
+      console.log("/refresh/get-refresh---------------------------");
+
+      // 1. Get jwt
+      const jwtPayload = req.jwtPayload as Dict<any>;
+
+      // 2. Verify jwt
+      const { userId } = jwtPayload;
+
+      // 3. Add userId to access session
+      req.session.userId = userId;
+
+      return res.send("Look for cookie");
+    } catch (err) {
+      console.log(err);
+      await destroySession(req);
+      return res.status(401).send("Unauthenticated");
+    }
+  },
+];
