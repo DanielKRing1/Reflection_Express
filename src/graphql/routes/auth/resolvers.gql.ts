535a77d0c7ef22a410cb8d0eab6b8a8d3ba41b55
Daniel Ring <DanielKRing1@gmail.com>
2023-04-08 23:08:06 -0700

    Remove gql user mutations -> handled by express routes now

--- src/graphql/routes/auth/resolvers.gql.ts
+++ src/graphql/routes/auth/resolvers.gql.ts
@@ -1,84 +1,24 @@
 import dummyData from "../../dummyData";
 import GqlContext from "../../types/context.types";
 import { ResolverFragment } from "../../types/schema.types";
-import { UserArgs, CreateUserArgs, LoginArgs, User } from "./schema.gql";
+import { UserArgs, CreateUserArgs } from "./schema.gql";
 
 import compare from "../../../auth/password/compare";
+import prisma from "../../../prisma/client";
+import { User } from "@prisma/client";
 
 const resolvers = {
-  Query: {
-    user: (
-      _: undefined,
-      { email }: UserArgs,
-      contextValue: GqlContext,
-      info: undefined
-    ): User => {
-      return dummyData.Users[email] || null;
+    Query: {
+        user: (
+            _: undefined,
+            { email }: UserArgs,
+            contextValue: GqlContext,
+            info: undefined
+        ): User | null => {
+            return dummyData.Users[email] || null;
+        },
     },
-  },
-  Mutation: {
-    createUser: async (
-      _: undefined,
-      { email, password }: CreateUserArgs,
-      contextValue: GqlContext,
-      info: undefined
-    ): Promise<boolean> => {
-      try {
-        if (dummyData.Users[email] !== undefined)
-          throw new Error(
-            `createUser() received an invalid request to create a user that already exists ${email}`
-          );
-
-        // 1. Create User
-        const name: string = new Date().toISOString();
-        dummyData.Users[email] = {
-          name,
-          email,
-          lastUsedJId: null,
-        };
-
-        // 2. Create Password
-        dummyData.Passwords[email] = {
-          userId: email,
-          passwordHash: email,
-        };
-
-        return await resolvers.Mutation.login(
-          _,
-          { email, password },
-          contextValue,
-          info
-        );
-      } catch (err) {
-        return false;
-      }
-    },
-    login: async (
-      _: undefined,
-      { email, password }: CreateUserArgs,
-      contextValue: GqlContext,
-      info: undefined
-    ): Promise<boolean> => {
-      try {
-        if (dummyData.Users[email] !== undefined)
-          throw new Error(
-            `createUser() received an invalid request to create a user that already exists ${email}`
-          );
-
-        // 1. Get user password hash
-        const hash: string = dummyData.Passwords[email].passwordHash;
-
-        // 2. Authenticate password
-        const isAuthentic: boolean = await compare(password, hash);
-
-        // 3. Create session for user
-
-        return true;
-      } catch (err) {
-        return false;
-      }
-    },
-  },
+    Mutation: {},
 } as ResolverFragment;
 
 export default resolvers;
--- src/graphql/routes/auth/schema.gql.ts
+++ src/graphql/routes/auth/schema.gql.ts
@@ -11,55 +11,52 @@ const LOGIN_MUTATION_NAME = "login";
 // To be used in express auth middleware
 //    to prevent auth checks for these gql operation names
 export const AUTH_OP_NAMES: Set<string> = new Set([
-  USER_QUERY_NAME.toLocaleLowerCase(),
-  CREATE_USER_MUTATION_NAME.toLocaleLowerCase(),
-  LOGIN_MUTATION_NAME.toLocaleLowerCase(),
+    USER_QUERY_NAME.toLocaleLowerCase(),
+    CREATE_USER_MUTATION_NAME.toLocaleLowerCase(),
+    LOGIN_MUTATION_NAME.toLocaleLowerCase(),
 ]);
 
 export default {
-  Types: `
+    Types: `
     type User {
       name: String!
       email: String!
       lastUsedJId: String!
     }
   `,
-  Query: `
+    Query: `
     ${USER_QUERY_NAME}(email: String!): User
   `,
-  Mutation: `
-    ${CREATE_USER_MUTATION_NAME}(email: String!, password: String!): boolean!
-    ${LOGIN_MUTATION_NAME}(email: String!, password: String!): boolean!
-  `,
+    Mutation: ``,
 } as SchemaFragment;
 
 // QUERY RESOLVERS
 
 export type UserArgs = {
-  email: string;
+    email: string;
 };
 
 // MUTATION RESOLVERS
 
 export type CreateUserArgs = {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 };
 
 export type LoginArgs = {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 };
 
 // TYPES
 
 export type User = {
-  name: string;
-  email: string;
-  lastUsedJId: string | null;
+    name: string;
+    email: string;
+    lastUsedJId: string | null;
 };
 
 export type Password = {
-  userId: string;
-  passwordHash: string;
+    userId: string;
+    passwordHash: string;
 };
