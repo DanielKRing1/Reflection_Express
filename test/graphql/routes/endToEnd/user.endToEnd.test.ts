3f4ffed50948ef80dae6635d33ad33c392e17455
Daniel Ring <DanielKRing1@gmail.com>
2023-04-13 14:32:13 -0700

    User Tests working Test Creating User + Getting access cookie + Require access cookie to get User

---
+++ test/graphql/routes/endToEnd/user.endToEnd.test.ts
@@ -0,0 +1,151 @@
+import http from "http";
+import supertest from "supertest";
+
+import setCookie from "set-cookie-parser";
+
+import server from "../../../../src/index";
+import prisma from "../../../../src/prisma/client";
+import { ACCESS_SESSION_COOKIE_NAME } from "../../../../src/middlewares/session/access";
+import { REFRESH_SESSION_COOKIE_NAME } from "../../../../src/middlewares/session/refresh";
+
+const USER_ID = "1";
+const PASSWORD = "123";
+
+describe("User GraphQL server", () => {
+    let s: http.Server;
+    let agent: supertest.SuperAgentTest;
+
+    beforeAll(async () => {
+        await prisma.$queryRaw`TRUNCATE TABLE "User" CASCADE;`;
+
+        s = await server;
+        agent = supertest.agent(s);
+    });
+
+    it("Should get access and refresh session cookies (at least 2 cookies)", async () => {
+        const res = await agent
+            .post("/login/create-user")
+            .send({ userId: USER_ID, password: PASSWORD });
+
+        const cookies = setCookie
+            .parse(res as any)
+            .sort((a, b) => a.name.localeCompare(b.name));
+        expect(cookies.length).toBeGreaterThanOrEqual(2);
+        expect(cookies[0].name).toBe(ACCESS_SESSION_COOKIE_NAME);
+        expect(cookies[1].name).toBe(REFRESH_SESSION_COOKIE_NAME);
+    });
+
+    it("Should get the created User (this requires successfully sending an access session cookie with the request)", async () => {
+        const queryData = {
+            query: `query User {
+                user {
+                    email
+                    lastUsedJId
+                }
+            }`,
+            variables: { email: USER_ID },
+        };
+
+        const res = await agent.post("/graphql").send(queryData);
+
+        expect(res.text).toMatchSnapshot();
+    });
+
+    it("Should fail to get the created User without an access cookie", async () => {
+        // Reset agent (and cookies)
+        agent = supertest.agent(s);
+
+        // Perform test
+        const queryData = {
+            query: `query User {
+                user {
+                    email
+                    lastUsedJId
+                }
+            }`,
+            variables: { email: USER_ID },
+        };
+
+        const res = await agent.post("/graphql").send(queryData);
+
+        expect(res.text).toMatchSnapshot();
+    });
+
+    it("Should login and then get the created User", async () => {
+        await agent
+            .post("/login")
+            .send({ userId: USER_ID, password: PASSWORD });
+
+        // Perform test
+        const queryData = {
+            query: `query User {
+                user {
+                    email
+                    lastUsedJId
+                }
+            }`,
+            variables: { email: USER_ID },
+        };
+
+        const res = await agent.post("/graphql").send(queryData);
+
+        expect(res.text).toMatchSnapshot();
+    });
+
+    it("Should fail login with wrong password and then fail to get the created User without an access cookie", async () => {
+        // Reset agent (and cookies)
+        agent = supertest.agent(s);
+
+        // Unauthenticated login
+        await agent
+            .post("/login")
+            .send({ userId: USER_ID, password: "wrong password" });
+
+        // Perform test
+        const queryData = {
+            query: `query User {
+                user {
+                    email
+                    lastUsedJId
+                }
+            }`,
+            variables: { email: USER_ID },
+        };
+
+        const res = await agent.post("/graphql").send(queryData);
+
+        expect(res.text).toMatchSnapshot();
+    });
+
+    it("Should fail login with wrong userId and then fail to get the created User without an access cookie", async () => {
+        // Reset agent (and cookies)
+        agent = supertest.agent(s);
+
+        // Unauthenticated login
+        await agent
+            .post("/login")
+            .send({ userId: "wrong userId", password: PASSWORD });
+
+        // Perform test
+        const queryData = {
+            query: `query User {
+                user {
+                    email
+                    lastUsedJId
+                }
+            }`,
+            variables: { email: USER_ID },
+        };
+
+        const res = await agent.post("/graphql").send(queryData);
+
+        expect(res.text).toMatchSnapshot();
+    });
+
+    afterAll(async () => {
+        await prisma.$queryRaw`TRUNCATE TABLE "User" CASCADE;`;
+
+        await prisma.$disconnect();
+        await s.close();
+    });
+});
--- test/graphql/routes/integration/user.integration.test.ts
+++ test/graphql/routes/integration/user.integration.test.ts
@@ -1,8 +1,9 @@
+import http from "http";
 import supertest from "supertest";
 
 import { ApolloServer } from "@apollo/server";
 
-import app from "../../../../src/server";
+import server from "../../../../src/index";
 import typeDefs from "../../../../src/graphql/schema";
 import resolvers from "../../../../src/graphql/resolvers";
 import GqlContext from "../../../../src/graphql/types/context.types";
@@ -11,44 +12,38 @@ import prisma from "../../../../src/prisma/client";
 const USER_ID = "1";
 const PASSWORD = "123";
 
-describe("User api", () => {
+const gqlServer = new ApolloServer<GqlContext>({
+    typeDefs,
+    resolvers,
+});
+
+describe("User GraphQL api", () => {
+    let s: http.Server;
+    let request: supertest.SuperTest<supertest.Test>;
+
     beforeAll(async () => {
-        console.log(
-            `TRUNCATE TABLE ${process.env.DATABASE_SCHEMA}."User" CASCADE;`
-        );
         await prisma.$queryRaw`TRUNCATE TABLE "User" CASCADE;`;
 
         // TEST EXPRESS SESSION ROUTES
-        await supertest(app)
+        s = await server;
+        request = supertest(s);
+
+        await request
             .post("/login/create-user")
             .send({ userId: USER_ID, password: PASSWORD });
-
-        console.log("1");
-        // await supertest(app).post("/login/refresh");
     });
 
     it("Should get the created User", async () => {
-        console.log("2");
-        // // TEST EXPRESS SESSION ROUTES
-        // supertest(app).post("/login/create-user");
-        // supertest(app).post("/login/refresh");
-
-        // TEST GRAPHQL
-        const gqlServer = new ApolloServer<GqlContext>({
-            typeDefs,
-            resolvers,
-        });
-
         // run the query against the server and snapshot the output
         const res = await gqlServer.executeOperation(
             {
-                query: `query User($email: String!) {
-                    user(email: $email) {
+                query: `query User {
+                    user {
                         email
                         lastUsedJId
                     }
                 }`,
-                variables: { email: USER_ID },
+                variables: {},
             },
             {
                 contextValue: {
@@ -60,22 +55,19 @@ describe("User api", () => {
             }
         );
 
-        console.log(res);
-        // @ts-ignore
-        console.log(res.body.singleResult.data);
-        console.log(JSON.stringify(res.body));
+        // console.log(res);
+        // // @ts-ignore
+        // console.log(res.body.singleResult.data);
+        // console.log(JSON.stringify(res.body));
 
-        // Note the use of Node's assert rather than Jest's expect; if using
-        // TypeScript, `assert`` will appropriately narrow the type of `body`
-        // and `expect` will not.
-        // assert(response.body.kind === 'single');
-        // expect(response.body.singleResult.errors).toBeUndefined();
-        // expect(response.body.singleResult.data?.hello).toBe('Hello
-
-        //   expect(res).toMatchSnapshot();
+        expect(res).toMatchSnapshot();
     });
 
     afterAll(async () => {
+        await prisma.$queryRaw`TRUNCATE TABLE "User" CASCADE;`;
+
         await prisma.$disconnect();
+
+        await s.close();
     });
 });
