9e5d45babcc9e1e97bf46428851f07a0bb1778fb
Daniel Ring <DanielKRing1@gmail.com>
2023-04-13 14:32:22 -0700

    Format file

--- src/middlewares/jwt/verifyJwt.middleware.ts
+++ src/middlewares/jwt/verifyJwt.middleware.ts
@@ -9,40 +9,40 @@ import { Dict } from "../../types/global.types";
  * and attaches the jwt payload to req.jwtPayload
  */
 type JwtBody = {
-  jwt: string;
+    jwt: string;
 };
 export type JwtReq = {
-  jwt: Dict<any>;
+    jwt: Dict<any>;
 };
 export default async (
-  req: Request<{}, {}, JwtBody>,
-  res: Response,
-  next: NextFunction
+    req: Request<{}, {}, JwtBody>,
+    res: Response,
+    next: NextFunction
 ) => {
-  try {
-    // 1. Get jwt
-    const jwt: string = req.body.jwt;
+    try {
+        // 1. Get jwt
+        const jwt: string = req.body.jwt;
 
-    // 2. No jwt
-    if (jwt === undefined)
-      throw new Error(
-        `verify.middleware() did not find a "jwt" attribute in req.payload`
-      );
+        // 2. No jwt
+        if (jwt === undefined)
+            throw new Error(
+                `verify.middleware() did not find a "jwt" attribute in req.payload`
+            );
 
-    // 3. Get jwt payload
-    // May throw error if invalid
-    const payload: Dict<any> = await verifyJwt(
-      jwt,
-      process.env.JWT_SECRET as string
-    );
+        // 3. Get jwt payload
+        // May throw error if invalid
+        const payload: Dict<any> = await verifyJwt(
+            jwt,
+            process.env.JWT_SECRET as string
+        );
 
-    // 4. Attach 'jwt' to req object
-    req.jwtPayload = payload;
+        // 4. Attach 'jwt' to req object
+        req.jwtPayload = payload;
 
-    // 5. Next
-    return next();
-  } catch (err) {
-    console.log(err);
-    return res.status(403).send("Unauthorized");
-  }
+        // 5. Next
+        return next();
+    } catch (err) {
+        console.log(err);
+        return res.status(403).send("Unauthorized");
+    }
 };
