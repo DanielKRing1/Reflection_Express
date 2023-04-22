93ea0fcbe18e087a54a5ffea80742675c8957de9
Daniel Ring <DanielKRing1@gmail.com>
2023-04-22 14:55:09 -0700

    Formatting

--- src/graphql/scalars/date.gql.ts
+++ src/graphql/scalars/date.gql.ts
@@ -9,58 +9,59 @@ const CUSTOM_SCALAR_NAME = "DateTime";
 // DEFINITION
 
 const datetimeScalar = new GraphQLScalarType({
-  name: "DateTime",
-  description: "DateTime custom scalar type",
-  serialize(value): number {
-    // 1. ISO String -> cast to Date
-    if (
-      instanceOf(value, InstanceType.String) &&
-      instanceOf(value, InstanceType.ISO)
-    )
-      value = new Date(value as string);
-    // 2. Date obj -> return ms
-    if (instanceOf(value, InstanceType.Date)) return (value as Date).getTime(); // Convert outgoing Date to integer for JSON
-    // 3. Number -> return raw number
-    if (instanceOf(value, InstanceType.Number)) return value as number;
+    name: "DateTime",
+    description: "DateTime custom scalar type",
+    serialize(value): number {
+        // 1. ISO String -> cast to Date
+        if (
+            instanceOf(value, InstanceType.String) &&
+            instanceOf(value, InstanceType.ISO)
+        )
+            value = new Date(value as string);
+        // 2. Date obj -> return ms
+        if (instanceOf(value, InstanceType.Date))
+            return (value as Date).getTime(); // Convert outgoing Date to integer for JSON
+        // 3. Number -> return raw number
+        if (instanceOf(value, InstanceType.Number)) return value as number;
 
-    throw Error(
-      "GraphQL Date Scalar serializer expected a `ISO string | Date | number` data type"
-    );
-  },
-  parseValue(value): Date {
-    // 1. ISO String -> return Date
-    if (
-      instanceOf(value, InstanceType.String) &&
-      instanceOf(value, InstanceType.ISO)
-    )
-      return new Date(value as string);
-    // 2. Number -> return Date
-    if (instanceOf(value, InstanceType.Number))
-      return new Date(value as number); // Convert incoming integer to Date
+        throw Error(
+            "GraphQL Date Scalar serializer expected a `ISO string | Date | number` data type"
+        );
+    },
+    parseValue(value): Date {
+        // 1. ISO String -> return Date
+        if (
+            instanceOf(value, InstanceType.String) &&
+            instanceOf(value, InstanceType.ISO)
+        )
+            return new Date(value as string);
+        // 2. Number -> return Date
+        if (instanceOf(value, InstanceType.Number))
+            return new Date(value as number); // Convert incoming integer to Date
 
-    throw new Error(
-      "GraphQL Date Scalar parser expected a `ISO string | number`"
-    );
-  },
-  parseLiteral(ast) {
-    if (ast.kind === Kind.INT) {
-      // Convert hard-coded AST string to integer and then to Date
-      return new Date(parseInt(ast.value, 10));
-    }
-    if (ast.kind === Kind.STRING) {
-      // Convert hard-coded AST string to integer and then to Date
-      return new Date(ast.value);
-    }
-    // Invalid hard-coded value (not an integer)
-    return null;
-  },
+        throw new Error(
+            "GraphQL Date Scalar parser expected a `ISO string | number`"
+        );
+    },
+    parseLiteral(ast) {
+        if (ast.kind === Kind.INT) {
+            // Convert hard-coded AST string to integer and then to Date
+            return new Date(parseInt(ast.value, 10));
+        }
+        if (ast.kind === Kind.STRING) {
+            // Convert hard-coded AST string to integer and then to Date
+            return new Date(ast.value);
+        }
+        // Invalid hard-coded value (not an integer)
+        return null;
+    },
 });
 
 // EXPORT
 
 export default {
-  Schema: `scalar ${CUSTOM_SCALAR_NAME}`,
-  Resolver: {
-    [CUSTOM_SCALAR_NAME]: datetimeScalar,
-  },
+    Schema: `scalar ${CUSTOM_SCALAR_NAME}`,
+    Resolver: {
+        [CUSTOM_SCALAR_NAME]: datetimeScalar,
+    },
 } as ScalarFragment;
