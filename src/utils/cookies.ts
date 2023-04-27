87304df2349d95faa5fc9fa33fd71ca8bcc8a8be
Daniel Ring <DanielKRing1@gmail.com>
2023-04-27 15:17:51 -0700

    Create cookie util to clear cookies from browser + formatting

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -2,27 +2,31 @@ import { Response } from "express";
 import setCookie from "set-cookie-parser";
 
 export type HttpCookieResponse = {
-  status: number;
-  headers: {
-    "set-cookie": string;
-  } & any;
+    status: number;
+    headers: {
+        "set-cookie": string;
+    } & any;
 };
 export const mergeCookies = (
-  httpResponse: HttpCookieResponse,
-  expressRes: Response
+    httpResponse: HttpCookieResponse,
+    expressRes: Response
 ) => {
-  if (httpResponse.status >= 400)
-    throw new Error(
-      `mergeCookies() received an httpResponse with status code: ${httpResponse.status}`
+    if (httpResponse.status >= 400)
+        throw new Error(
+            `mergeCookies() received an httpResponse with status code: ${httpResponse.status}`
+        );
+
+    // 6.3. Strip refresh cookie from response
+    // Get all cookies from cookie header
+    const cookies = setCookie.parse(httpResponse as any);
+    console.log(cookies);
+
+    // 6.4. Attach refresh cookie to res.cookie
+    cookies.forEach((cookie) =>
+        expressRes.cookie(cookie.name, cookie.value, cookie as any)
     );
+};
 
-  // 6.3. Strip refresh cookie from response
-  // Get all cookies from cookie header
-  const cookies = setCookie.parse(httpResponse as any);
-  console.log(cookies);
-
-  // 6.4. Attach refresh cookie to res.cookie
-  cookies.forEach((cookie) =>
-    expressRes.cookie(cookie.name, cookie.value, cookie as any)
-  );
+export const clearCookieFromBrowser = (cookieName: string, res: Response) => {
+    res.clearCookie(cookieName, { expires: new Date(0) });
 };
