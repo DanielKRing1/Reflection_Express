2786fee9aed0a71d98577d9219c6fc9d4caab91e
Daniel Ring <DanielKRing1@gmail.com>
2023-04-27 15:16:05 -0700

    Create gql error handler that destorys session when user unauthenticated

---
+++ src/graphql/error/catch.ts
@@ -0,0 +1,40 @@
+import { GraphQLError } from "graphql";
+
+import {
+    UNAUTHENTICATED_GQL_ERROR_CODE,
+    genInternalServerError,
+} from "./errorGenerators";
+import {
+    SessionCookieType,
+    destroySession,
+} from "../../middlewares/session/utils";
+import GqlContext from "../types/context.types";
+
+/**
+ * Returns original GraphQL error or creates a new one from error message
+ * Also clears access cookie session, since user is no longer authenticated but seems to think they are
+ *
+ * @param err
+ * @param res
+ * @returns
+ */
+export const createResolverError = (
+    err: any,
+    gqlContext: GqlContext
+): GraphQLError => {
+    console.log(err);
+
+    // 1. Clear Access session cookies if unauthenticated
+    if (
+        err instanceof GraphQLError &&
+        err.extensions.code === UNAUTHENTICATED_GQL_ERROR_CODE
+    )
+        destroySession(
+            gqlContext.req,
+            gqlContext.res,
+            SessionCookieType.Access
+        );
+
+    // 2. Return GraphQL error
+    return err instanceof GraphQLError ? err : genInternalServerError(err);
+};
