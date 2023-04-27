5ea4e1deeda15b9d32a2a3c12199c1297e2745c1
Daniel Ring <DanielKRing1@gmail.com>
2023-04-27 15:18:06 -0700

    Formatting

--- src/middlewares/session/genAuthorizeSession.middleware.ts
+++ src/middlewares/session/genAuthorizeSession.middleware.ts
@@ -11,22 +11,22 @@ import genSessionMiddleware from "./genSession.middleware";
  *  and instead, it will be more clear that sessions can simply be added but that they may also need to be 'authorized'
  */
 export default (sessionMiddleware: MiddlewareFunc): MiddlewareFunc[] => [
-  genSessionMiddleware(sessionMiddleware),
-  (req, res, next) => {
-    try {
-      // 1. Did not receive a valid session, throw error
-      const { userId }: SessionData = req.session;
-      if (userId === undefined)
-        throw new Error(
-          "genAuthorizeSession middleware did not find a session for the received request: terminate request"
-        );
+    genSessionMiddleware(sessionMiddleware),
+    (req, res, next) => {
+        try {
+            // 1. Did not receive a valid session, throw error
+            const { userId }: SessionData = req.session;
+            if (userId === undefined)
+                throw new Error(
+                    "genAuthorizeSession middleware did not find a session for the received request: terminate request"
+                );
 
-      // 2. Received a valid session
-      next();
-    } catch (err) {
-      console.log(err);
+            // 2. Received a valid session
+            next();
+        } catch (err) {
+            console.log(err);
 
-      return res.status(401).send("Unauthenticated");
-    }
-  },
+            return res.status(401).send("Unauthenticated");
+        }
+    },
 ];
