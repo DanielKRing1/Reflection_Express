76a2d2700ed3556b5c260f26e0deff9e58f1b4db
Daniel Ring <DanielKRing1@gmail.com>
2023-05-24 19:45:36 -0700

    New build -> 1.1.0

--- build-docker.sh
+++ build-docker.sh
@@ -3,5 +3,6 @@
 npm run bump-minor
 npm run compile
 docker build -t registry.fly.io/reflection-api:$npm_package_version .
+docker push registry.fly.io/reflection-api:$npm_package_version .
 
 $SHELL
