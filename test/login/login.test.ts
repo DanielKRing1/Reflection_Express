492f5eeb30a0260d982111a41dee24402b935cfd
Daniel Ring <DanielKRing1@gmail.com>
2023-06-28 15:50:00 -0700

    Create login/logout tests

---
+++ test/login/login.test.ts
@@ -0,0 +1,98 @@
+import http from "http";
+import supertest from "supertest";
+
+import setCookie from "set-cookie-parser";
+
+import server from "../../src/index";
+import prisma from "../../src/prisma/client";
+import {
+    ACCESS_SESSION_COOKIE_NAME,
+    META_ACCESS_SESSION_COOKIE_NAME,
+} from "../../src/middlewares/session/access/constants";
+import {
+    REFRESH_SESSION_COOKIE_NAME,
+    META_REFRESH_SESSION_COOKIE_NAME,
+} from "../../src/middlewares/session/refresh/constants";
+
+const USER_ID = "1";
+const PASSWORD = "123";
+
+describe("User GraphQL server", () => {
+    let s: http.Server;
+    let agent: supertest.SuperAgentTest;
+
+    beforeAll(async () => {
+        await prisma.$queryRaw`TRUNCATE TABLE "User" CASCADE;`;
+
+        s = await server;
+        agent = supertest.agent(s);
+    });
+
+    it("Should get access and refresh session cookies (at least 2 cookies)", async () => {
+        const res = await agent
+            .post("/login/create-user")
+            .send({ userId: USER_ID, password: PASSWORD });
+
+        const cookies = setCookie
+            .parse(res as any)
+            .sort((a, b) => a.name.localeCompare(b.name));
+        console.log(cookies);
+        expect(cookies.length).toBe(4);
+
+        expect(cookies[0].name).toBe(ACCESS_SESSION_COOKIE_NAME);
+        expect(cookies[1].name).toBe(META_ACCESS_SESSION_COOKIE_NAME);
+        expect(cookies[2].name).toBe(REFRESH_SESSION_COOKIE_NAME);
+        expect(cookies[3].name).toBe(META_REFRESH_SESSION_COOKIE_NAME);
+    });
+
+    it("Should logout", async () => {
+        let res;
+        let cookies;
+
+        res = await agent.post("/login/logout").send();
+        cookies = setCookie
+            .parse(res as any)
+            .sort((a, b) => a.name.localeCompare(b.name));
+        console.log(cookies);
+        // expect(cookies.length).toBe(4);
+        expect(
+            agent.jar.getCookies({
+                path: "/",
+                domain: ".asianpersonn.top",
+                secure: false,
+                script: false,
+            }).length
+        ).toBe(2);
+
+        res = await agent.post("/refresh/logout").send();
+        cookies = setCookie
+            .parse(res as any)
+            .sort((a, b) => a.name.localeCompare(b.name));
+        console.log(cookies);
+        // expect(cookies.length).toBe(4);
+
+        console.log(
+            agent.jar.getCookies({
+                path: "/",
+                domain: ".asianpersonn.top",
+                secure: false,
+                script: false,
+            })
+        );
+        expect(
+            agent.jar.getCookies({
+                path: "/",
+                domain: ".asianpersonn.top",
+                secure: false,
+                script: false,
+            }).length
+        ).toBe(0);
+    });
+
+    afterAll(async () => {
+        await prisma.$queryRaw`TRUNCATE TABLE "User" CASCADE;`;
+
+        await prisma.$disconnect();
+        await s.close();
+    });
+});
