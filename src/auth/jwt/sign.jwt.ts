037bd34ed75b75ad5f6933beade46e966768b7d0
Daniel Ring <DanielKRing1@gmail.com>
2023-06-28 22:42:57 -0700

    Formatting

--- src/auth/jwt/sign.jwt.ts
+++ src/auth/jwt/sign.jwt.ts
@@ -2,33 +2,33 @@ import jwt from "jsonwebtoken";
 import { Dict } from "../../types/global.types";
 
 type SignOptions = {
-  algorithm?: jwt.Algorithm;
-  expiresIn?: number;
+    algorithm?: jwt.Algorithm;
+    expiresIn?: number;
 };
 export default async (
-  payload: Dict<any>,
-  secret: string,
-  { algorithm = "HS256", expiresIn = 0 }: SignOptions = {}
+    payload: Dict<any>,
+    secret: string,
+    { algorithm = "HS256", expiresIn = 0 }: SignOptions = {}
 ) => {
-  // 1. Await token generation
-  const token: string | undefined = await new Promise<string | undefined>(
-    (res, rej) =>
-      jwt.sign(
-        payload,
-        secret,
-        { algorithm, expiresIn },
-        function (err, token) {
-          if (err) return res(undefined);
+    // 1. Await token generation
+    const token: string | undefined = await new Promise<string | undefined>(
+        (res, rej) =>
+            jwt.sign(
+                payload,
+                secret,
+                { algorithm, expiresIn },
+                function (err, token) {
+                    if (err) return res(undefined);
 
-          return res(token);
-        }
-      )
-  );
+                    return res(token);
+                }
+            )
+    );
 
-  // 2. Throw error if undefined token
-  if (token === undefined)
-    throw new Error("sign.jwt() produced an undefined token");
+    // 2. Throw error if undefined token
+    if (token === undefined)
+        throw new Error("sign.jwt() produced an undefined token");
 
-  // 3. Return valid token
-  return token;
+    // 3. Return valid token
+    return token;
 };
--- src/auth/jwt/verify.jwt.ts
+++ src/auth/jwt/verify.jwt.ts
@@ -2,21 +2,21 @@ import jwt from "jsonwebtoken";
 import { Dict } from "../../types/global.types";
 
 export default async (token: string, secret: string) => {
-  // 1. Await payload decoding
-  const payload: Dict<any> | undefined = await new Promise<
-    Dict<any> | undefined
-  >((res, rej) =>
-    jwt.verify(token, secret, function (err, payload) {
-      if (err) return res(undefined);
+    // 1. Await payload decoding
+    const payload: Dict<any> | undefined = await new Promise<
+        Dict<any> | undefined
+    >((res, rej) =>
+        jwt.verify(token, secret, function (err, payload) {
+            if (err) return res(undefined);
 
-      return res(payload as Dict<any>);
-    })
-  );
+            return res(payload as Dict<any>);
+        })
+    );
 
-  // 2. Throw error if undefined payload
-  if (payload === undefined)
-    throw new Error("verify.jwt() produced an undefined payload");
+    // 2. Throw error if undefined payload
+    if (payload === undefined)
+        throw new Error("verify.jwt() produced an undefined payload");
 
-  // 3. Return valid payload
-  return payload;
+    // 3. Return valid payload
+    return payload;
 };
